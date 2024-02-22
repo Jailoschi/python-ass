6f2a5b73b7b938df183778c8c9fcf0342d149d45
Gh0sTed <113895444+ProfessorMB21@users.noreply.github.com>
2024-02-22 23:15:17 +0300

    add new func

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
 //Pythonic functions are defined by the reserved word "def"
 // followed by the name of the function "def function_name:"
 
-def function:
+def function():  //simple function
   pass
+
+"""
+  Functions can also accept parameters/arguments
+  We use the reserved word 'return' to output 
+  whatever the function was doing in its body
+"""
+def calculate_area(length, width):  
+  """calculates the area of a rectangle and returns the output"""
+  area = length * width
+  return area
+  
